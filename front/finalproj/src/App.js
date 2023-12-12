a90be8704d6292b5ec0fb592a1a7517ae3dfa847
KimWonjong123 <bonk159@gmail.com>
2023-12-12 23:33:36 +0900

    front register with email verification

--- front/finalproj/src/App.js
+++ front/finalproj/src/App.js
@@ -6,19 +6,20 @@ import About from "./pages/About";
 import UserList from "./pages/User";
 import Login from "./pages/Login";
 import Logout from "./pages/Logout";
+import Register from "./pages/Register";
 
 function App() {
     return (
         <div className="App">
             <nav>
                 <Link to="/">Home</Link> | <Link to="/about">About</Link> |{" "}
-                <Link to="/user">Users</Link> | <Link to="login">Login</Link> |{" "}
+                <Link to="/register">Register</Link> | <Link to="login">Login</Link> |{" "}
                 <Link to="/logout">Logout</Link>
             </nav>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
-                <Route path="/user" element={<UserList />} />
+                <Route path="/register" element={<Register />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/logout" element={<Logout />} />
             </Routes>
---
+++ front/finalproj/src/pages/Register.js
@@ -0,0 +1,111 @@
+import { React, useState } from "react";
+import { setCookie } from "../util/cookie";
+import axios from "axios";
+
+function Register() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [emailCheck, setEmailCheck] = useState(false);
+    const [emailCode, setEmailCode] = useState("");
+
+    const handleNameChange = (e) => {
+        setName(e.target.value);
+    }
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handleEmailCodeChange = (e) => {
+        setEmailCode(e.target.value);
+    };
+
+    const handleEmailCheck = (e) => {
+        e.preventDefault();
+        axios
+            .post("http://localhost:8080/api/email/verify", {
+                email: email,
+            })
+            .then((res) => {
+                setEmailCheck(res.data.success);
+                console.log(res.data.success);
+                const emailInput = document.querySelector(
+                    'input[type="email"]'
+                );
+                emailInput.disabled = true;
+            })
+            .catch((err) => {
+                console.log(err.response.data.message);
+            });
+    };
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+        if (emailCheck !== true || emailCode === "") {
+            console.log("Please check your email.");
+            console.log(emailCheck);
+            console.log(emailCode);
+            return;
+        }
+        if (password === "" || name === "") {
+            console.log("Please check your password or name.");
+            return;
+        }
+        axios
+            .post("http://localhost:8080/api/users/register", {
+                name: name,
+                email: email,
+                rawPassword: password,
+                code: emailCode,
+            })
+            .then((res) => {
+                setCookie("token", res.data.token, { path: "/" });
+                window.location.href = "/";
+            })
+            .catch((err) => {
+                console.log(err.response.data.message);
+            });
+    };
+
+    return (
+        <div className="Register">
+            <header>
+                <h1>Register Page</h1>
+                <h2>hello</h2>
+                <input
+                    type="text"
+                    onChange={handleNameChange}
+                    placeholder="Enter your name."
+                    required={true}
+                />
+                <input
+                    type="email"
+                    onChange={handleEmailChange}
+                    placeholder="Enter your Email."
+                    required={true}
+                />
+                <button onClick={handleEmailCheck}>Check Email</button>
+                <input
+                    type="text"
+                    onChange={handleEmailCodeChange}
+                    placeholder="Enter your Email verification code"
+                    required={true}
+                />
+                <input
+                    type="password"
+                    onChange={handlePasswordChange}
+                    placeholder="Enter your password."
+                    required={true}
+                />
+                <button onClick={handleRegister}>Register</button>
+            </header>
+        </div>
+    );
+}
+
+export default Register;
