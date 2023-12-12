2d2b9a3a9a80baea11e651e5becc92360c08de99
KimWonjong123 <bonk159@gmail.com>
2023-12-12 15:18:35 +0900

    backend init

---
+++ back/webproj-final/src/main/java/weblab/finalproj/WebprojFinalApplication.java
@@ -0,0 +1,13 @@
+package weblab.finalproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebprojFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebprojFinalApplication.class, args);
+    }
+
+}
---
+++ back/webproj-final/src/test/java/weblab/finalproj/WebprojFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package weblab.finalproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebprojFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
